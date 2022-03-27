2266c2e88e093a223c3a0b06ef7bf349fa842620
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuelhurtado20@hotmail.com>
2022-03-26 22:42:39 -0500

    produce product

---
+++ IMS.UseCases/ProduceProductUseCase.cs
@@ -0,0 +1,36 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases
+{
+    public class ProduceProductUseCase
+    {
+        private readonly IInventoryRepository repository;
+        private readonly IProductRepository productRepository;
+        private readonly IInventoryTransactionRepository inventoryTransactionRepository;
+        private readonly IProductTransactionRepository productTransactionRepository;
+
+        public ProduceProductUseCase(IInventoryRepository inventoryRepository, 
+            IProductRepository productRepository, 
+            IInventoryTransactionRepository inventoryTransactionRepository, IProductTransactionRepository productTransactionRepository)
+        {
+            this.repository = inventoryRepository;
+            this.productRepository = productRepository;
+            this.inventoryTransactionRepository = inventoryTransactionRepository;
+            this.productTransactionRepository = productTransactionRepository;
+        }
+
+        public async Task ExecuteAsync(string productionNumber, Product product, int quantity, string doneBy)
+        {
+            await this.productTransactionRepository.ProduceAsync(productionNumber, product, quantity, doneBy);
+
+            product.Quantity += quantity;
+            await this.productRepository.UpdateProductAsync(product);
+        }
+    }
+}
